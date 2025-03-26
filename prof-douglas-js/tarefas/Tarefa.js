11c8367a63401d51faefe1a76d13018d46741e14
unknown <Administrador@GI-COL-TADS-09.integrado.int>
2025-03-25 22:51:35 -0300

    e

---
+++ prof-douglas-js/tarefas/Tarefa.js
@@ -0,0 +1,11 @@
+console.log("o resultado do a é " + (+2+3*4/2));
+
+console.log("o resultado do B é " + (9+3*2));
+
+console.log("o resultado do c é " + ((9+3)*2));
+
+console.log("o resultado da d é " + ((3-5*4)/5));
+
+console.log("o resultado do E é " + (3+7* Math.pow(2,5)));
+
+console.log("o resultado da f é" + (Math.sqrt (2+8/5)));
---
+++ prof-douglas-js/tarefas/exercicio.js
@@ -0,0 +1,8 @@
+const leitura = require(prompt-sync)();
+
+let a = leitura("informe o valor de a: ")
+let b = leitura("informe o valor de b: ")
+let c = leitura("informe o valor de c: ")
+let d = leitura("informe o valor de d: ")
+
+console.log("resultado: " + (d = a + c - b));
