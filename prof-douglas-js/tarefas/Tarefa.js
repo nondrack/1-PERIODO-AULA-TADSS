b0afc911fab2b53e100af4fdcf2ab6a8f3c33358
nondrack <luckasfernando500@gmail.com>
2025-03-26 01:00:10 -0300

    aula

--- prof-douglas-js/tarefas/Tarefa.js
+++ prof-douglas-js/tarefas/Tarefa.js
@@ -8,4 +8,16 @@ console.log("o resultado da d é " + ((3-5*4)/5));
 
 console.log("o resultado do E é " + (3+7* Math.pow(2,5)));
 
-console.log("o resultado da f é" + (Math.sqrt (2+8/5)));
+console.log("o resultado da f é " + (Math.sqrt (2+8/5)));
+
+console.log("o resultado da g é " + (Math.pow(5,2) - 4 * 2 * 8));
+
+console.log("o resultado da h é " + (((3+4*3)*5)-3));
+
+console.log("o resultado da i é " + ((2*3+4*2)+4*(5+3)));
+
+console.log("o resultado da j é " + (Math.pow(5,3)-3*Math.pow(4,2)));
+
+console.log("o resultado da k é " + (38+15-100*2+150));
+
+console.log("o resultado da l é " + (((2+4*3)/7)+3*9));
--- prof-douglas-js/tarefas/exercicio.js
+++ prof-douglas-js/tarefas/exercicio.js
@@ -1,8 +1,8 @@
-const leitura = require(prompt-sync)();
+const leitura = require("prompt-sync")();
 
-let a = leitura("informe o valor de a: ")
-let b = leitura("informe o valor de b: ")
-let c = leitura("informe o valor de c: ")
-let d = leitura("informe o valor de d: ")
+let a = leitura("informe o valor de a: ");
+let b = leitura("informe o valor de b: ");
+let c = leitura("informe o valor de c: ");
+let d = ((a) + (c) - (b));
 
-console.log("resultado: " + (d = a + c - b));
+console.log(d);
