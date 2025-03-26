646c2fca227a41c0f3cb5ab8013387a2805d6cd3
unknown <Administrador@GI-COL-TADS-09.integrado.int>
2025-03-26 19:52:42 -0300

    aula

--- prof-douglas-js/tarefas/exercicio.js
+++ prof-douglas-js/tarefas/exercicio.js
@@ -1,8 +1,7 @@
-const leitura = require(prompt-sync)();
+const leitura = require("prompt-sync")();
 
-let a = leitura("informe o valor de a: ")
-let b = leitura("informe o valor de b: ")
-let c = leitura("informe o valor de c: ")
-let d = leitura("informe o valor de d: ")
+let a = leitura("informe o valor de a: ");
+let b = leitura("informe o valor de b: ");
+let c = leitura("informe o valor de c: ");
 
-console.log("resultado: " + (d = a + c - b));
+console.log("D = " + (( a + c )- b));
