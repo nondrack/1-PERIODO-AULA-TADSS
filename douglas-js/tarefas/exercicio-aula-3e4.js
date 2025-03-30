e8c91e7fc004bbd247aa50819a7e18d00ad473e8
nondrack <luckasfernando500@gmail.com>
2025-03-29 21:30:28 -0300

    ex aula 3e4

---
+++ douglas-js/tarefas/exercicio-aula-3e4.js
@@ -0,0 +1,34 @@
+const prompt = require("prompt-sync")();
+
+// EX:1 
+
+// let nome = prompt("Digite um nome: ");
+// let altura = prompt("Digite sua altura em Metros: ");
+// let peso = prompt("Digite seu peso em Kg: ");
+
+// let pesoIdeal = (peso/(altura*altura));
+
+// if(pesoIdeal < 17){
+//     console.log(nome + " sua Faixa de peso é " + pesoIdeal +" e você Esta muito abaixo do peso");
+// }
+// if((pesoIdeal >= 17) && (pesoIdeal <= 18.49)){
+//     console.log(nome + " sua Faixa de peso é " + pesoIdeal +" e você esta abaixo do peso");
+// }
+// if((pesoIdeal >= 18.5) && (pesoIdeal <= 24.99)){
+//     console.log(nome + " sua Faixa de peso é " + pesoIdeal +" e você Esta com peso normal");
+// }
+// if((pesoIdeal >= 25)&& (pesoIdeal <= 29.99)){
+//     console.log(nome + " sua Faixa de peso é " + pesoIdeal +" e você Esta acima do peso ");
+// }
+// if((pesoIdeal >= 30)&& (pesoIdeal<= 34.99)){
+//     console.log(nome + " sua Faixa de peso é " + pesoIdeal +" e você Esta com Obesidade 1");
+// }
+// if((pesoIdeal >= 35)&& (pesoIdeal<= 39.99)){
+//     console.log(nome + " sua Faixa de peso é " + pesoIdeal +" e você Esta com Obesidade 2 " + "(Severa)");
+// }
+// if(pesoIdeal >= 40){
+//     console.log(nome + " sua Faixa de peso é " + pesoIdeal +" e você Esta Obesidade 3 " + "(mórbida)");
+// }
+
+//EX: 2
+
